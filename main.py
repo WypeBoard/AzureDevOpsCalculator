08452abc3f9e6fb490e3ba130ad23efa1ca0b8f7
Nikolaj Bové Højholt <nbh@netcompany.com>
2025-03-03 20:58:16 +0100

    Created Logger class and added log statements

--- main.py
+++ main.py
@@ -2,34 +2,41 @@ from AzureDevopsClient import AzureDevopsClient
 import Constants
 from azure_devops_request_url import RequestHelper
 from calculations import PullRequestAgeCalculations, PullRequestInactiveCalculations, PullRequestWrongTargetBranchCalculations
+from logger import Logger
 from model import PullRequest, PullRequests, Thread
 from Manager import Manager
 from tqdm import tqdm
 
 
 def main():
+    log = Logger(__name__)
+    log.info("Starting script")
+    log.debug("Initializing ADO client and fetching data")
     request_helper = RequestHelper(Constants.BASE_URL, Constants.ORGANISATION, Constants.PROJECT_NAME)
     client = AzureDevopsClient(request_helper, Constants.REPOSITORY_NAME, Constants.TOKEN) 
     # Initialize base dataclass for all pull request data
     pullrequests: list[PullRequests] = []
     # Fetch all pull requests
+    log.debug("Fetching pull requests")
     pullrequest_data = fetch_all_pull_requests(client)
+    log.debug(f"Fetched {len(pullrequest_data)} pull requests")
     # Map to class with pydantic
     pullrequest_list = [PullRequest.model_validate(obj) for obj in pullrequest_data]
     
     for pr in tqdm(pullrequest_list, desc='Fetching data for pull requests'):
         threads = fetch_threads_for_pull_request(client, pr)
         pullrequests.append(PullRequests(id=pr.pullRequestId, base=pr, threads=threads))
-    
+    log.info("Data from ADO has been fetched")
     # Initialize statistic manager
+    log.info("Generating Manager class")
     manager = Manager(pullrequests)
-    
     # Register calculations
+    log.debug("Registering calculations")
     manager.register_calculation(PullRequestAgeCalculations.PullRequestAgeCalculations)
     manager.register_calculation(PullRequestInactiveCalculations.PullRequestInactiveCalculation)
     manager.register_calculation(PullRequestWrongTargetBranchCalculations.PullRequestWrongTargetBranchCalculations)
-    
     # Run calculations
+    log.info("Starting manager execution")
     manager.execute()
 
 def fetch_threads_for_pull_request(client, pr: PullRequest) -> list[Thread]:
